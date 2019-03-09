c31548fdc09ffdd73be1f2db4b4478d7b63ec2fe
nempak <nempak@gmail.com>
2019-03-09 17:29:25 +0200

    Added html

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,8 +13,27 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/open-iconic-bootstrap.min.css',
+        'css/css/animate.css',
+        'css/magnific-popup.css',
+        'css/aos.css',
+        'css/ionicons.min.css',
+        'css/flaticon.css',
+        'css/icomoon.css',
+        'css/style.css',
+        'https://fonts.googleapis.com/css?family=Great+Vibes',
+        'https://fonts.googleapis.com/css?family=Josefin+Sans:400,700',
+        'https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700',
     ];
     public $js = [
+        'js/popper.min.js',
+        'js/bootstrap.min.js',
+        'js/jquery.easing.1.3.js',
+        'js/jquery.waypoints.min.js',
+        'js/jquery.stellar.min.js',
+        'js/aos.js',
+        'js/scrollax.min.js',
+        'js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -4,11 +4,7 @@
 /* @var $content string */
 
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
-use common\widgets\Alert;
 
 AppAsset::register($this);
 ?>
@@ -25,57 +21,249 @@ AppAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
-
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
-
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+        <a class="navbar-brand" href="index.html"> <img src="images/dadfries.png" alt="" style="width: 150px;"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="oi oi-menu"></span> Menu
+        </button>
+        <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active"><a href="index.html" class="nav-link">Главная</a></li>
+                <li class="nav-item"><a href="menu.html" class="nav-link">Оставить отзыв</a></li>
+                <li class="nav-item">
+                    <div class="dp-header__item no-ma">
+                        <div class="dp-cart-item">
+                            <div class="dp-cart-wrapper">
+                                <div class="dp-button-cart">
+                                    <div class="dp-button-cart__icon-wrap">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 510 510">
+                                            <path d="M153,408c-28.05,0-51,22.95-51,51s22.95,51,51,51s51-22.95,51-51S181.05,408,153,408z M0,0v51h51l91.8,193.8L107.1,306 c-2.55,7.65-5.1,17.85-5.1,25.5c0,28.05,22.95,51,51,51h306v-51H163.2c-2.55,0-5.1-2.55-5.1-5.1v-2.551l22.95-43.35h188.7 c20.4,0,35.7-10.2,43.35-25.5L504.9,89.25c5.1-5.1,5.1-7.65,5.1-12.75c0-15.3-10.2-25.5-25.5-25.5H107.1L84.15,0H0z M408,408 c-28.05,0-51,22.95-51,51s22.95,51,51,51s51-22.95,51-51S436.05,408,408,408z">
+                                            </path>
+                                        </svg>
+                                    </div>
+                                    <div class="dp-button-cart__price-block">0.00 р.</div>
+                                    <div class="dp-button-cart__text-block">
+                                        <a href="cart.html"><span>Заказать</span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
-</div>
-
-<footer class="footer">
+</nav>
+<section class="ftco-menu">
     <div class="container">
-        <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+        <!--<div class="row justify-content-center">-->
+        <!--<div class="col-md-7 heading-section text-center ftco-animate">-->
+        <!--&lt;!&ndash;<h2 class="mb-4">Меню</h2>&ndash;&gt;-->
+        <!--<p>Бургеры | Фритюрные закуски | Напитки | Сладости из Европы</p>-->
+        <!--</div>-->
+        <!--</div>-->
+        <div class="row d-md-flex">
+            <div class="col-lg-12 ftco-animate p-md-5">
+                <div class="row">
+                    <div class="col-md-12 nav-link-wrap mb-5">
+                        <div class="nav ftco-animate nav-pills justify-content-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                            <a class="nav-link active" id="v-pills-1-tab" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-1" aria-selected="true">Бургеры</a>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+                            <a class="nav-link" id="v-pills-3-tab" data-toggle="pill" href="#v-pills-3" role="tab" aria-controls="v-pills-3" aria-selected="false">Закуски</a>
+
+                            <a class="nav-link" id="v-pills-2-tab" data-toggle="pill" href="#v-pills-2" role="tab" aria-controls="v-pills-2" aria-selected="false">Напитки</a>
+
+                            <a class="nav-link" id="v-pills-4-tab" data-toggle="pill" href="#v-pills-4" role="tab" aria-controls="v-pills-4" aria-selected="false">Сладости из Европы</a>
+
+                        </div>
+                    </div>
+                    <div class="col-md-12 d-flex align-items-center">
+
+                        <div class="tab-content ftco-animate" id="v-pills-tabContent">
+
+                            <div class="tab-pane fade show active" id="v-pills-1" role="tabpanel" aria-labelledby="v-pills-1-tab">
+                                <div class="row">
+                                    <div class="col-md-4 text-center">
+                                        <div class="menu-wrap">
+                                            <a href="#" class="menu-img img mb-4" style="background-image: url(images/burger_1.jpg);"></a>
+                                            <div class="text">
+                                                <h3><a href="#">Завод</a></h3>
+                                                <p>С индейкой, сыром чеддер, перечным соусом и томатами</p>
+                                                <p class="price"><span>250 р.</span></p>
+                                                <p><a href="#" class="btn btn-primary btn-outline-primary">Добавить в корзину</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 text-center">
+                                        <div class="menu-wrap">
+                                            <a href="#" class="menu-img img mb-4" style="background-image: url(images/burger_2.jpg);"></a>
+                                            <div class="text">
+                                                <h3><a href="#">Папа Бургер</a></h3>
+                                                <p>С красным луком, томатами, маринованным огурцом, сыром чеддер, двойным беконом, луковым конфитюром, перечным соусом и котлетой на Ваш выбор (говядина / индейка)</p>
+                                                <p class="price"><span>380 р.</span></p>
+                                                <p><a href="#" class="btn btn-primary btn-outline-primary">Добавить в корзину</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 text-center">
+                                        <div class="menu-wrap">
+                                            <a href="#" class="menu-img img mb-4" style="background-image: url(images/burger_3.jpg);"></a>
+                                            <div class="text">
+                                                <h3><a href="#">Папа Жарит</a></h3>
+                                                <p></p>
+                                                <p class="price"><span>200 р.</span></p>
+                                                <p><a href="#" class="btn btn-primary btn-outline-primary">Добавить в корзину</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="tab-pane fade" id="v-pills-2" role="tabpanel" aria-labelledby="v-pills-2-tab">
+                                <div class="row">
+                                    <div class="col-md-4 text-center">
+                                        <div class="menu-wrap">
+                                            <a href="#" class="menu-img img mb-4" style="background-image: url(images/drink-1.jpg);"></a>
+                                            <div class="text">
+                                                <h3><a href="#">Lemonade Juice</a></h3>
+                                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+                                                <p class="price"><span>$2.90</span></p>
+                                                <p><a href="#" class="btn btn-primary btn-outline-primary">Add to cart</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 text-center">
+                                        <div class="menu-wrap">
+                                            <a href="#" class="menu-img img mb-4" style="background-image: url(images/drink-2.jpg);"></a>
+                                            <div class="text">
+                                                <h3><a href="#">Pineapple Juice</a></h3>
+                                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+                                                <p class="price"><span>$2.90</span></p>
+                                                <p><a href="#" class="btn btn-primary btn-outline-primary">Add to cart</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 text-center">
+                                        <div class="menu-wrap">
+                                            <a href="#" class="menu-img img mb-4" style="background-image: url(images/drink-3.jpg);"></a>
+                                            <div class="text">
+                                                <h3><a href="#">Soda Drinks</a></h3>
+                                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+                                                <p class="price"><span>$2.90</span></p>
+                                                <p><a href="#" class="btn btn-primary btn-outline-primary">Add to cart</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="tab-pane fade" id="v-pills-3" role="tabpanel" aria-labelledby="v-pills-3-tab">
+                                <div class="row">
+                                    <div class="col-md-4 text-center">
+                                        <div class="menu-wrap">
+                                            <a href="#" class="menu-img img mb-4" style="background-image: url(images/dessert-1.jpg);"></a>
+                                            <div class="text">
+                                                <h3><a href="#">Hot Cake Honey</a></h3>
+                                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+                                                <p class="price"><span>$2.90</span></p>
+                                                <p><a href="#" class="btn btn-primary btn-outline-primary">Add to cart</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 text-center">
+                                        <div class="menu-wrap">
+                                            <a href="#" class="menu-img img mb-4" style="background-image: url(images/dessert-2.jpg);"></a>
+                                            <div class="text">
+                                                <h3><a href="#">Hot Cake Honey</a></h3>
+                                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+                                                <p class="price"><span>$2.90</span></p>
+                                                <p><a href="#" class="btn btn-primary btn-outline-primary">Add to cart</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 text-center">
+                                        <div class="menu-wrap">
+                                            <a href="#" class="menu-img img mb-4" style="background-image: url(images/dessert-3.jpg);"></a>
+                                            <div class="text">
+                                                <h3><a href="#">Hot Cake Honey</a></h3>
+                                                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+                                                <p class="price"><span>$2.90</span></p>
+                                                <p><a href="#" class="btn btn-primary btn-outline-primary">Add to cart</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="ftco-gallery">
+    <div class="container-wrap">
+        <div class="row no-gutters">
+            <div class="col-md-3 ftco-animate">
+                <a href="gallery.html" class="gallery img d-flex align-items-center" style="background-image: url(images/gallery-1.jpg);">
+                    <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                        <span class="icon-search"></span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 ftco-animate">
+                <a href="gallery.html" class="gallery img d-flex align-items-center" style="background-image: url(images/gallery-2.jpg);">
+                    <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                        <span class="icon-search"></span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 ftco-animate">
+                <a href="gallery.html" class="gallery img d-flex align-items-center" style="background-image: url(images/gallery-3.jpg);">
+                    <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                        <span class="icon-search"></span>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 ftco-animate">
+                <a href="gallery.html" class="gallery img d-flex align-items-center" style="background-image: url(images/gallery-4.jpg);">
+                    <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                        <span class="icon-search"></span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<footer class="ftco-footer ftco-section img">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-12 col-md-12 ">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Контакты</h2>
+                    <div class="block-23 mb-3">
+                        <ul>
+                            <li  style="height: 34px;"><span class="icon icon-map-marker"></span><span class="text">г. Конаково, ул. Васильковского 29</span></li>
+                            <li  style="height: 34px;"><a href="#"><span class="icon icon-phone"></span><span class="text">8 965 723 51 61</span></a></li>
+                            <li  style="height: 34px;"><a href="https://www.instagram.com/p/BrSHB58HnTG/"><span class="icon icon-instagram"></span><span class="text">dadfries_burgers</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+
+                <p style="font-size: 10px;">
+                    &copy;<script>document.write(new Date().getFullYear());</script> ВСЕ ПРАВА ЗАЩИЩЕНЫ!. ЛЮБОЕ ИСПОЛЬЗОВАНИЕ МАТЕРИАЛОВ ДОПУСКАЕТСЯ ТОЛЬКО С СОГЛАСИЯ РЕДАКЦИИ.
+                </p>
+            </div>
+        </div>
     </div>
 </footer>
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 <?php $this->endBody() ?>
 </body>
