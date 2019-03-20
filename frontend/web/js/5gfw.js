053e7a2c0e3d7ec7b1c10c9aa99221320f66c2ed
nempak <nempak@gmail.com>
2019-03-20 18:52:02 +0200

    Started with classes

--- frontend/web/js/5gfw.js
+++ frontend/web/js/5gfw.js
@@ -1,46 +1,85 @@
-let add_to_cart_button = $('.add-to-cart');
+/*global $*/
 
-let product_plus = $('[data-attr="product-buttons"] [data-attr="plus"]');
+
 
 // $.cookie("order", null, { path: '/' });
 // $.cookie("order", [], {expires: 1, path: '/'});
+//
+// add_to_cart_button.on('click', function () {
+//
+//     $(this).hide();
+//
+//     increaseOrder($(this).data('id'));
+//
+//     $('#product-' + $(this).data('id')).find('[data-attr="product-buttons"]').show();
+//
+//     console.log($.cookie("order"));
+// });
+//
+// product_plus.on('click', function () {
+//
+//     console.log($(this));
+//
+//     increaseOrder($(this).parent('[data-attr="product-buttons"]').data('id'));
+//
+// });
+//
+//
+// function increaseOrder(order_id) {
+//
+//     let current_count = $('#product-' + order_id).find('[data-attr="product-buttons"]').find('[data-attr="count"]').text();
+//
+//     current_count = parseInt(current_count) + 1;
+//
+//     console.log(current_count);
+//
+//     $('#product-' + order_id).find('[data-attr="product-buttons"]').find('[data-attr="count"]').text(current_count);
+//
+//     let current_order = JSON.parse($.cookie('order'));
+//
+//     current_order.order_id = current_count;
+//
+//     $.cookie('order', JSON.stringify(current_order));
+//
+//     console.log(JSON.parse($.cookie('order')));
+//
+// }
+//
+// console.log(JSON.parse($.cookie('order')));
 
-add_to_cart_button.on('click', function () {
 
-    $(this).hide();
+class Cart {
 
-    increaseOrder($(this).data('id'));
+    constructor () {
 
-    $('#product-' + $(this).data('id')).find('[data-attr="product-buttons"]').show();
+        this.add_to_cart_button = $('.add-to-cart');
 
-    console.log($.cookie("order"));
-});
+        this.product_plus = $('[data-attr="product-buttons"] [data-attr="plus"]');
 
-product_plus.on('click', function () {
+        this.product_minus = $('[data-attr="product-buttons"] [data-attr="minus"]');
 
-    console.log($(this));
+        this.events();
+    }
 
-    increaseOrder($(this).parent('[data-attr="product-buttons"]').data('id'));
+    increaseProductInCart(event) {
+        console.log(event.target.getAttribute('data-id'));
+        $(this).hide();
+        $('#product-' + event.target.getAttribute('data-id')).find('[data-attr="product-buttons"]').show();
+    }
 
-});
+    decreaseProductInCart(event) {
+        console.log(event.target.getAttribute('data-id'));
+    }
 
+    events () {
 
-function increaseOrder(order_id) {
+        this.add_to_cart_button.on('click', this.increaseProductInCart);
 
-    let current_count = $('#product-' + order_id).find('[data-attr="product-buttons"]').find('[data-attr="count"]').text();
+        this.product_plus.on('click', this.increaseProductInCart);
 
-    current_count = parseInt(current_count) + 1;
+        this.product_minus.on('click', this.decreaseProductInCart);
 
-    console.log(current_count);
-
-    $('#product-' + order_id).find('[data-attr="product-buttons"]').find('[data-attr="count"]').text(current_count);
-
-    let current_order = JSON.parse($.cookie('order'));
-
-    $.cookie('order', JSON.stringify({'id' : order_id, 'count' : current_count}));
-
-    console.log(JSON.parse($.cookie('order')));
-
+    }
 }
 
-console.log(JSON.parse($.cookie('order')));
+let cart = new Cart();
